7593bf9ee264b96896976a01aef3ffe9c9c70a78
Shepherd <neiferjr15@gmail.com>
2025-10-18 12:13:51 -0500

    feat: initialize Go project with basic health check and Swagger documentation
    - Added .gitignore to exclude binaries and IDE files
    - Implemented router for handling health check and Swagger documentation endpoints
    - Created Swagger documentation files (JSON and YAML)
    - Set up Go module with necessary dependencies
    - Added health check handler to return server status
    - Configured application settings with environment variables

---
+++ api/router/router.go
@@ -0,0 +1,48 @@
+package router
+
+import (
+	"net/http"
+
+	"Encargalo.app-api.go/internal/health/handler"
+	"Encargalo.app-api.go/shared/config"
+	echoSwagger "github.com/swaggo/echo-swagger"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Router struct {
+	server *echo.Echo
+	config config.Config
+}
+
+func New(
+	server *echo.Echo,
+	config config.Config,
+) *Router {
+	return &Router{
+		server,
+		config,
+	}
+}
+
+func (r *Router) Init() {
+
+	r.server.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "remote_ip=${remote_ip}, method=${method}, uri=${uri}, status=${status}, latency=${latency_human}\n\n",
+	}))
+
+	r.server.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins:                             []string{"*"},
+		AllowMethods:                             []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete, http.MethodOptions},
+		AllowHeaders:                             []string{echo.HeaderContentType},
+		AllowCredentials:                         true,
+		UnsafeWildcardOriginWithAllowCredentials: true,
+	}))
+
+	r.server.Use(middleware.Recover())
+
+	r.server.GET("/health", handler.HealthCheck)
+	r.server.GET("/docs/*", echoSwagger.EchoWrapHandler())
+
+}
