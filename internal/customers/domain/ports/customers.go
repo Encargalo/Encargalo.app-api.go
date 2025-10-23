c2ddca0735cf17e5349e6a2bb42d1e1df1c606cb
Shepherd <neiferjr15@gmail.com>
2025-10-23 00:02:22 -0500

    se agg la funcionalidad para que se inicie sesion cuando se cree un usuario

--- internal/customers/domain/ports/customers.go
+++ internal/customers/domain/ports/customers.go
@@ -9,14 +9,14 @@ import (
 )
 
 type CustomersApp interface {
-	RegisterCustomer(ctx context.Context, customer dto.RegisterCustomer) (uuid.UUID, error)
+	RegisterCustomer(ctx context.Context, customer dto.RegisterCustomer) error
 	SearchCustomerBy(ctx context.Context, criteria dto.SearchCustomerBy) (*models.Accounts, error)
 	UpdateCustomer(ctx context.Context, customer_id uuid.UUID, customer dto.UpdateCustomer) error
 	UpdatePassword(ctx context.Context, customer_id uuid.UUID, pass dto.UpdatePassword) error
 }
 
 type CustomersRepo interface {
-	RegisterCustomer(ctx context.Context, customer *models.Accounts) (*models.Accounts, error)
+	RegisterCustomer(ctx context.Context, customer *models.Accounts) error
 	SearchCustomerBy(ctx context.Context, criteria dto.SearchCustomerBy) (*models.Accounts, error)
 	SearchCustomerByPhoneAndNotIDEquals(ctx context.Context, customer_id uuid.UUID, phone string) (*models.Accounts, error)
 	UpdateCustomer(ctx context.Context, customer_id uuid.UUID, customer *models.Accounts) error
